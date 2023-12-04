e987cbf0a71c821663ffb88f8a85680d7e9a897c
Fernando Dingler <ferdingler@users.noreply.github.com>
2023-12-04 13:53:37 -0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { InferGetServerSidePropsType } from 'next';
 import Link from 'next/link';
+import packageJson from "next/package.json";
 
 type HomeProps = InferGetServerSidePropsType<typeof getServerSideProps>;
 
@@ -11,6 +12,8 @@ const Home = (props: HomeProps) => {
         <Link href="/page1">Page 1</Link>
         <Link href="/page2">Page 2</Link>
       </div>
+
+      <b>NextJS deployed version {packageJson.version}</b>
 
       <h1>Server Side Props</h1>
       <pre>{JSON.stringify(props, null, 2)}</pre>
